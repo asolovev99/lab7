84211101613e7bb86ca9b3a7b0381e59107884e5
Andrew Solovev <solovevaa@mer.ci.nsu.ru>
2020-12-25 00:44:59 +0700

    ...

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -10,17 +10,14 @@ int main()
 	int index_min;
 	int i = 0;
 
-	//srand(time(0));
 	for (i = 0; i < length_array; i++)
 	{
-		//numbers[i] = (int)rand();
 		scanf("%d", &numbers[i]);
 	}
 
 	printf("Current permutation:\n");
 	for (i = 0; i < length_array; i++)
 	{
-		//numbers[i] = (int)rand();
 		printf("%d: %d\n", i, numbers[i]);
 	}
 
